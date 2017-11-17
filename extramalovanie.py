d461bed89027fca53484376ba1b90579f21d3731
Elena Suranova <elena.suranova@seiteq.com>
2017-11-17 22:12:37 +0100

    doplnenie textu

--- extramalovanie.py
+++ extramalovanie.py
@@ -4,4 +4,4 @@ vyska = int (input ("zadaj vysku: "))
 litrem2 = int(input ("zadaj litre na m2: "))
 celkovom2 = dlzka * vyska + sirka * vyska + dlzka * vyska + sirka * vyska + sirka * dlzka
 celkovolitrov = celkovom2 * litrem2
-print (celkovolitrov)
+print ("na vymalovanie celej miestnosti potrebujes", celkovolitrov, "litrov farby")
