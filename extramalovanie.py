1dc376597697e05f79e1ed04fbbd50fc8e79d507
Elena Suranova <elena.suranova@seiteq.com>
2017-11-17 21:53:15 +0100

    napisany cely program

---
+++ extramalovanie.py
@@ -0,0 +1,7 @@
+dlzka = int (input ("zadaj dlzku: "))
+sirka = int (input ("zadaj sirku: "))
+vyska = int (input ("zadaj vysku: "))
+litrem2 = int(input ("zadaj litre na m2: "))
+celkovom2 = dlzka * vyska + sirka * vyska + dlzka * vyska + sirka * vyska + sirka * dlzka
+celkovolitrov = celkovom2 * litrem2
+print (celkovolitrov)
