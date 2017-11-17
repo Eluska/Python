a23809933f3dbdb24dff36bfc1f055ece456da58
Elena Suranova <elena.suranova@seiteq.com>
2017-11-17 21:28:02 +0100

    prva verzia programu

--- malovanie.py
+++ malovanie.py
@@ -1,5 +1,7 @@
-print ("hi there")
-
-
-
-####
+dlzka = 3
+sirka = 10
+vyska = 4
+litrem2 = 2
+celkovom2 = dlzka * vyska + sirka * vyska + dlzka * vyska + sirka * vyska + sirka * dlzka
+celkovolitrov = celkovom2 * litrem2
+print (celkovolitrov)
